2b2ac37cba30aa88736000eb2f2734faa494a648
Cristy Koebler <cristy.koebler@gmail.com>
2014-09-23 09:37:02 -0400

    Sorcery authentication

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -3,4 +3,9 @@ module ArticlesHelper
 	def article_params
 		params.require(:article).permit(:title, :body, :tag_list, :image)
 	end
+
+	def tag_links(tags)
+		links = tags.collect{|tag| link_to tag.name, tag_path(tag)}
+		return links.join(", ").html_safe
+	end
 end
