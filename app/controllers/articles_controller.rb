b871252cab9087265adfd038c0c8ea8c9341d96b
Cristy Koebler <cristy.koebler@gmail.com>
2014-10-15 10:31:04 -0400

    Updated login and style

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
 	include ArticlesHelper
 
 	def index
-		@articles = Article.all
+		@articles = Article.all.order("created_at DESC")
 	end
 
 	def show
